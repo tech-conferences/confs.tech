aadb4c254029bc51fcf0cb3bd35ae94215c1e371
Nima Izadi <nima.izadi@shopify.com>
2017-09-11 08:55:07 -0400

    Add tests to check duplicates and remove duplicates

---
+++ src/components/App/tests/utils.js
@@ -0,0 +1,14 @@
+export function getDuplicates(conferences) {
+  const confURLs = conferences.map((conf) => conf.url);
+  const duplicates = [];
+
+  Object.keys(conferences).forEach((key, index) => {
+    const url = conferences[key].url;
+    if (confURLs.indexOf(url, index + 1) !== -1) {
+      if (duplicates.indexOf(url) === -1) {
+        duplicates.push(conferences[key]);
+      }
+    }
+  });
+  return duplicates;
+}
