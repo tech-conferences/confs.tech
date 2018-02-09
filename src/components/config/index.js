4140af0e4d7d60a393740177757b46c558bb6424
Nima Izadi <nim.izadi@gmail.com>
2018-02-09 18:12:52 -0500

    Moved PHP section before General

--- src/components/config/index.js
+++ src/components/config/index.js
@@ -3,11 +3,11 @@ export const CURRENT_YEAR = (new Date()).getFullYear() - 1;
 export const TYPES = {
   javascript: 'JavaScript',
   css: 'CSS',
-  php: 'PHP',
   ux: 'Design / UX',
   ruby: 'Ruby',
   ios: 'iOS / Swift',
   android: 'Android',
+  php: 'PHP',
   general: 'General',
 };
 
