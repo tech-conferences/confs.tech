fe734410494bc7ccb15facbb4fe2208fd5cfc334
Nima Izadi <nim.izadi@gmail.com>
2018-03-08 21:13:58 -0500

    Updated test utils

--- src/components/App/tests/utils.js
+++ src/components/App/tests/utils.js
@@ -1,10 +1,10 @@
 export function getDuplicates(conferences) {
-  const confURLs = conferences.map((conf) => conf.url);
+  const confUUIDs = conferences.map((conf) => `${conf.url}-${conf.city}`);
   const duplicates = [];
 
   Object.keys(conferences).forEach((key, index) => {
     const url = conferences[key].url;
-    if (confURLs.indexOf(url, index + 1) !== -1) {
+    if (confUUIDs.indexOf(url, index + 1) !== -1) {
       if (duplicates.indexOf(url) === -1) {
         duplicates.push(conferences[key]);
       }
