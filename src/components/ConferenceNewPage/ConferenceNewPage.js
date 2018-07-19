84d1d35d6fe601797c7a92a5064cdee09fcc8bc7
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-07-19 12:05:57 -0700

    Update repo from confs.tech to conference-data

--- src/components/ConferenceNewPage/ConferenceNewPage.js
+++ src/components/ConferenceNewPage/ConferenceNewPage.js
@@ -395,15 +395,15 @@ export default class ConferenceNewPage extends Component {
         </form>
 
         <Divider />
-        <Link external url="https://github.com/tech-conferences/confs.tech/pulls">
+        <Link external url="https://github.com/tech-conferences/conference-data/pulls">
           Pull requests
         </Link>
         {' – '}
-        <Link external url="https://github.com/tech-conferences/confs.tech/issues">
+        <Link external url="https://github.com/tech-conferences/conference-data/issues">
           Create an issue
         </Link>
         {' – '}
-        <Link external url="https://github.com/tech-conferences/confs.tech/">
+        <Link external url="https://github.com/tech-conferences/conference-data/">
           GitHub repository
         </Link>
         {' – '}
