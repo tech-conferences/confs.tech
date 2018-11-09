b4f7213b3a03bb73a12688fbaa74ce1652e4c599
Nima Izadi <nim.izadi@gmail.com>
2018-11-08 20:07:51 -0500

    Stop showing the banner if dismissed

--- src/components/StayTuned/StayTuned.tsx
+++ src/components/StayTuned/StayTuned.tsx
@@ -1,9 +1,12 @@
-import React, {PureComponent} from 'react';
+import React, {PureComponent, ComponentType} from 'react';
 import classNames from 'classnames';
+import {withCookies, ReactCookieProps} from 'react-cookie';
 
 import Link from '../Link';
 import Heading from '../Heading';
 import styles from './StayTuned.scss';
+
+const DISMISSED_COOKIE = 'dismissed-stay-tuned';
 
 interface Props {
   topic: string;
@@ -13,16 +16,20 @@ interface State {
   gdprChecked: boolean;
   closed: boolean;
 }
+type ComposedProps = Props & ReactCookieProps;
 
-export default class StayTuned extends PureComponent<Props, State> {
+class StayTuned extends PureComponent<ComposedProps, State> {
   state: State = {
     gdprChecked: false,
     closed: false,
-  }
+  };
 
   render() {
-    const {topic} = this.props;
+    const {cookies, topic} = this.props;
     const {gdprChecked, closed} = this.state;
+    if (cookies && cookies.get(DISMISSED_COOKIE)) {
+      return null;
+    }
 
     return (
       <div className={classNames(styles.StayTuned, closed && styles.hidden)}>
@@ -30,22 +37,48 @@ export default class StayTuned extends PureComponent<Props, State> {
           Stay tuned!
         </Heading>
         <div className={styles.Close} onClick={this.close}>
-          <Link button>
-            Close
-          </Link>
+          <Link button>Close</Link>
         </div>
         <div className={styles.Content}>
-          <form action="https://tech.us19.list-manage.com/subscribe/post?u=246492d8cf0efc8c4ec6a9a60&amp;id=84b8d4723e" method="post"
-            id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className={styles.Form} target="_blank" noValidate>
+          <form
+            action="https://tech.us19.list-manage.com/subscribe/post?u=246492d8cf0efc8c4ec6a9a60&amp;id=84b8d4723e"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            className={styles.Form}
+            target="_blank"
+            noValidate
+          >
             <div className={styles.FormFirstLine}>
               <div>
-                <input type="email" name="EMAIL" className={styles.EmailInput} id="mce-EMAIL" placeholder="Email"/>
-                <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true">
-                  <input type="text" name="b_246492d8cf0efc8c4ec6a9a60_84b8d4723e" tabIndex={-1} value={topic} />
+                <input
+                  type="email"
+                  name="EMAIL"
+                  className={styles.EmailInput}
+                  id="mce-EMAIL"
+                  placeholder="Email"
+                />
+                <div
+                  style={{position: 'absolute', left: '-5000px'}}
+                  aria-hidden="true"
+                >
+                  <input
+                    type="text"
+                    name="b_246492d8cf0efc8c4ec6a9a60_84b8d4723e"
+                    tabIndex={-1}
+                    value={topic}
+                  />
                 </div>
               </div>
               <div>
-                <input disabled={!gdprChecked} type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className={styles.Button} />
+                <input
+                  disabled={!gdprChecked}
+                  type="submit"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  className={styles.Button}
+                />
               </div>
             </div>
             <div className={styles.GDPR}>
@@ -58,7 +91,9 @@ export default class StayTuned extends PureComponent<Props, State> {
                 value="Y"
                 className="av-checkbox"
               />
-              <label htmlFor="gdpr_18847">I accept to receive emails – GDPR&nbsp;✅</label>
+              <label htmlFor="gdpr_18847">
+                I accept to receive emails – GDPR&nbsp;✅
+              </label>
             </div>
           </form>
           <Link url="https://twitter.com/ConfsTech/" external>
@@ -72,12 +107,18 @@ export default class StayTuned extends PureComponent<Props, State> {
   private handleGdprChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({
       gdprChecked: event.currentTarget.checked,
-    })
-  }
+    });
+  };
 
   private close = () => {
+    const {cookies} = this.props;
+    if (cookies) {
+      cookies.set(DISMISSED_COOKIE, true, {path: '/'});
+    }
     this.setState({
       closed: true,
-    })
-  }
+    });
+  };
 }
+
+export default withCookies(StayTuned) as ComponentType<Props>;
