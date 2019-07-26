64b70321764e16c9e25eb0aca12cd769a311b7fd
Nima Izadi <nim.izadi@gmail.com>
2019-07-26 10:59:18 -0400

    Use correct node_env

--- scripts/start.js
+++ scripts/start.js
@@ -4,8 +4,8 @@
 /* eslint-disable promise/always-return */
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = process.env.BABEL_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -31,7 +31,7 @@ const {
 } = require('react-dev-utils/WebpackDevServerUtils');
 const openBrowser = require('react-dev-utils/openBrowser');
 const paths = require('../config/paths');
-const config = require('../config/webpack.config.dev');
+const config = process.env.NODE_ENV === 'production' ? require('../config/webpack.config.prod') : require('../config/webpack.config.dev');
 const createDevServerConfig = require('../config/webpackDevServer.config');
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
