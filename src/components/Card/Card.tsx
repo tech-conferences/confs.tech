e5529a1f67b4ce2a07f65435d10400709852e012
Nima <nim.izadi@gmail.com>
2021-01-15 09:18:45 +0100

    Add Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Card.scss';
+
+const Card: React.FC = ({children}) => {
+  return (
+    <div className={styles.Card}>
+      {children}
+    </div>
+  );
+};
+export default Card;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Divider } from './Divider'
+export { default as Card } from './Card'
+export { default as Heading } from './Heading'
+export { default as Link } from './Link'
