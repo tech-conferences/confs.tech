8e6d2eb8a0badde36db9753d9ec405f18df402d4
Nima Izadi <nim.izadi@gmail.com>
2021-01-17 18:48:56 +0100

    Feature/add online field (#475)
    * Add online field in conference form
    
    * Put checkbox above and make city/country optional
    
    * Update react-datepicker
    
    * Add locationType dropdown

---
+++ src/components/InputGroup/InputGroup.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classNames from 'classnames'
+import * as styles from './InputGroup.scss'
+
+interface Props {
+  inline?: boolean
+}
+
+const InputGroup: React.FC<Props> = ({ children, inline }) => {
+  return (
+    <div className={classNames(styles.InputGroup, inline && styles.inline)}>
+      {children}
+    </div>
+  )
+}
+
+export default InputGroup
