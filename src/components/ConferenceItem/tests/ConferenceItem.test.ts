5a0c418f74af3689b5250c89e7d37d6492283f28
Nima Izadi <nim.izadi@gmail.com>
2021-03-09 10:07:52 +0000

    Fixed tests and updated jest

--- src/components/ConferenceItem/tests/ConferenceItem.test.ts
+++ src/components/ConferenceItem/tests/ConferenceItem.test.ts
@@ -5,21 +5,21 @@ import { formatDate } from '../utils'
 describe('#formatDate', () => {
   describe('with same startDate and endDate', () => {
     it('should return only with the start date', () => {
-      const date = formatDate('2018/01/01', '2018/01/01')
+      const date = formatDate('2018-01-01', '2018-01-01')
       expect(date).toBe('January 1')
     })
   })
 
   describe('with different startDate and endDate', () => {
     it('should return start and end date', () => {
-      const date = formatDate('2018/01/01', '2018/01/02')
+      const date = formatDate('2018-01-01', '2018-01-02')
       expect(date).toBe('January 1-2')
     })
   })
 
   describe('with dates without specific day', () => {
     it('should return only the month', () => {
-      const date = formatDate('2018/01', '2018/01')
+      const date = formatDate('2018-01', '2018-01')
       expect(date).toBe('January')
     })
   })
