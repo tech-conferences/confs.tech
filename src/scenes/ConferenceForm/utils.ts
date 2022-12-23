04669da1913c2017746b99fcb582dce66b186519
Christian Grail <christian.grail@sap.com>
2022-12-23 14:09:42 +0100

    Add more unwanted conference names and urls

--- src/scenes/ConferenceForm/utils.ts
+++ src/scenes/ConferenceForm/utils.ts
@@ -3,8 +3,8 @@ export const VALID_URL_REGEX = /^http(s?):\/\//
 export const URL_PARAMETER_REGEX = /\?/
 export const URL_SHORTENER_REGEX = /(\/bitly)|(\/bit\.ly)|(\/t\.co)/i
 export const TWITTER_REGEX = /@\w([\w\.]){1,15}$/
-export const UNWANTED_CONFERENCE_NAME_REGEX = /webinar|marketing|practical guide|meeting|trends|digimarcon|hackathon|101|estate|expo|techspo|outsourcing|physical|biology|neuroscience|healthcare|nutrition|Food Science/i
-export const UNWANTED_CONFERENCE_URL_REGEX = /webinar|marketing|hackathon|digimarcon/i
+export const UNWANTED_CONFERENCE_NAME_REGEX = /webinar|marketing|practical guide|meeting|trends|digimarcon|hackathon|101|estate|expo|techspo|outsourcing|physical|biology|neuroscience|health|nutrition|Food Science|Nanoscience|nursing|Sustainability|training|Agriculture|cancer|dentistry|banking/i
+export const UNWANTED_CONFERENCE_URL_REGEX = /webinar|marketing|hackathon|digimarcon|insightconferences|conferenceseries|inovineconferences|annualcongress/i
 
 export const isValidTwitterHandle = (twitterHandle: string) => {
   // Allow empty handle
