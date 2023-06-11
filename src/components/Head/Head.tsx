0b626516085938be281ddc4a6221e5ebb61665d1
Nima Izadi <nim.izadi@gmail.com>
2023-06-11 18:29:33 -0400

    Update to Google Analytics 4

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -56,16 +56,18 @@ const Head = () => {
           content='Find your next tech conference: JavaScript, UX / Design, DevOps, Android, iOS, PHP, Ruby, Python etc'
         />
         <meta name='twitter:image' content='/logo.png' />
-        <script type='text/javascript'>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-FBVEHTPVZC'
+        ></script>
+        <script>
           {`
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-              ga('create', 'UA-105233299-1', 'auto');
-              ga('set', 'anonymizeIp', true);
-              ga('send', 'pageview');
+          gtag('config', 'G-FBVEHTPVZC');
           `}
         </script>
         <link rel='icon' type='image/png' href={faviconUrl} />
