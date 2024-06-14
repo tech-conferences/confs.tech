f6dacc3095c49353334f81cfa3314e05b3e8f625
JuanPabloDiaz <juanchodis@hotmail.com>
2024-06-14 14:58:20 -0400

    Team Page #630

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,52 +1,57 @@
-import { Routes, Route, Navigate, useParams } from 'react-router-dom'
-import { AboutPage, NotFoundPage, SponsorshipPage } from 'src/StaticPages'
-import { ConferenceList, ConferenceForm } from 'src/scenes'
+import { Routes, Route, Navigate, useParams } from "react-router-dom";
+import {
+  AboutPage,
+  NotFoundPage,
+  SponsorshipPage,
+  TeamPage,
+} from "src/StaticPages";
+import { ConferenceList, ConferenceForm } from "src/scenes";
 
-import styles from './App.module.scss'
+import styles from "./App.module.scss";
 
 export default function App() {
   return (
     <div className={styles.AppContainer}>
       <div className={styles.App}>
         <Routes>
-          <Route path='/pages/:page' element={<Pages />} />
-          <Route path='/conferences/new' element={<ConferenceForm />} />
-          <Route path='/past' element={<ConferenceList showPast />} />
+          <Route path="/pages/:page" element={<Pages />} />
+          <Route path="/conferences/new" element={<ConferenceForm />} />
+          <Route path="/past" element={<ConferenceList showPast />} />
           <Route
-            path='/cfp/:topic/:country'
+            path="/cfp/:topic/:country"
             element={<ConferenceList showCFP />}
           />
-          <Route path='/cfp/:topic/' element={<ConferenceList showCFP />} />
-          <Route path='/cfp' element={<ConferenceList showCFP />} />
-          <Route path='/:year/:topic/:country' element={<HandleRedirect />} />
-          <Route path='/:topic/:country' element={<HandleRedirectOrRender />} />
-          <Route path='/:topic' element={<ConferenceList />} />
-          <Route path='/' element={<ConferenceList />} />
+          <Route path="/cfp/:topic/" element={<ConferenceList showCFP />} />
+          <Route path="/cfp" element={<ConferenceList showCFP />} />
+          <Route path="/:year/:topic/:country" element={<HandleRedirect />} />
+          <Route path="/:topic/:country" element={<HandleRedirectOrRender />} />
+          <Route path="/:topic" element={<ConferenceList />} />
+          <Route path="/" element={<ConferenceList />} />
           <Route element={<ConferenceList />} />
         </Routes>
       </div>
     </div>
-  )
+  );
 }
 
 function Pages() {
-  const params = useParams<keyof Params>() as Params
+  const params = useParams<keyof Params>() as Params;
 
   switch (params.page) {
-    case 'about':
-      return <AboutPage />
-    case 'team':
-      return <TeamPage />
-    case 'sponsorships':
-      return <SponsorshipPage />
+    case "about":
+      return <AboutPage />;
+    case "team":
+      return <TeamPage />;
+    case "sponsorships":
+      return <SponsorshipPage />;
   }
-  return <NotFoundPage />
+  return <NotFoundPage />;
 }
 
 function HandleRedirect() {
-  const { topic, country } = useParams<keyof Params>() as Params
+  const { topic, country } = useParams<keyof Params>() as Params;
 
-  return <Navigate to={`/${topic}/${country}`} />
+  return <Navigate to={`/${topic}/${country}`} />;
 }
 
 /*
@@ -55,24 +60,24 @@ function HandleRedirect() {
   the new route /:topic
 */
 interface Params {
-  page: string
-  topic: string
-  country: string
+  page: string;
+  topic: string;
+  country: string;
 }
 function HandleRedirectOrRender() {
-  const { topic, country } = useParams<keyof Params>() as Params
+  const { topic, country } = useParams<keyof Params>() as Params;
 
   if (isYear(topic)) {
-    return <Navigate to={`/${country}`} />
+    return <Navigate to={`/${country}`} />;
   } else {
-    return <ConferenceList />
+    return <ConferenceList />;
   }
 }
 
 function isYear(year?: string) {
   if (!year) {
-    return false
+    return false;
   }
 
-  return year.length === 4 && !isNaN(parseInt(year, 10))
+  return year.length === 4 && !isNaN(parseInt(year, 10));
 }
