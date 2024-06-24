ab4cc152eb10b6deae5211a0497abdf5d5ddfbc3
Nima Izadi <nim.izadi@gmail.com>
2024-06-24 19:42:14 -0400

    remove camelcase

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -2,11 +2,6 @@
 
 const path = require('path')
 
-const camelcase = require('camelcase')
-
-// This is a custom Jest transformer turning file imports into filenames.
-// http://facebook.github.io/jest/docs/en/webpack.html
-
 module.exports = {
   process(src, filename) {
     const assetFilename = JSON.stringify(path.basename(filename))
@@ -14,9 +9,7 @@ module.exports = {
     if (filename.match(/\.svg$/)) {
       // Based on how SVGR generates a component name:
       // https://github.com/smooth-code/svgr/blob/01b194cf967347d43d4cbe6b434404731b87cf27/packages/core/src/state.js#L6
-      const pascalCaseFilename = camelcase(path.parse(filename).name, {
-        pascalCase: true,
-      })
+      const pascalCaseFilename = path.parse(filename).name
       const componentName = `Svg${pascalCaseFilename}`
       return `const React = require('react');
       module.exports = {
