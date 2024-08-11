c3a117640ab20db304bec79a9aa51a3a828ca8a7
Nima Izadi <nim.izadi@gmail.com>
2024-08-11 18:04:21 -0400

    Revert "Revert "Adding express server to run app for heroku stack 22+ (#711)"…"
    This reverts commit e7ef3a86997252ad6881d2158f6f8bbcf3bda5a7.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node scripts/heroku-start.js
---
+++ scripts/heroku-start.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+// Your static pre-build assets folder
+app.use(express.static(path.join(__dirname, '..', 'build')))
+// Root Redirects to the pre-build assets
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build'))
+})
+// Any Page Redirects to the pre-build assets folder index.html that // will load the react app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+})
+app.listen(port, () => {
+  console.log('Server is running on port: ', port)
+})
