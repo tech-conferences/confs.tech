7b4f3223997fcd1db23a7195e4054deb771dd63d
Juan Diaz <juanchodis@hotmail.com>
2025-07-02 18:42:55 +0000

    fix

--- scripts/start.js
+++ scripts/start.js
@@ -115,38 +115,40 @@ checkBrowsers(paths.appPath, isInteractive)
       host: HOST,
       port,
     }
+
     const devServer = new WebpackDevServer(serverConfig, compiler)
     // Launch WebpackDevServer.
     devServer.startCallback(() => {
       if (isInteractive) {
-      clearConsole()
-    }
+        clearConsole()
+      }
 
-    if (env.raw.FAST_REFRESH && semver.lt(react.version, '16.10.0')) {
-      console.log(
-        chalk.yellow(
-          `Fast Refresh requires React 16.10 or higher. You are using React ${react.version}.`,
-        ),
-      )
-    }
+      if (env.raw.FAST_REFRESH && semver.lt(react.version, '16.10.0')) {
+        console.log(
+          chalk.yellow(
+            `Fast Refresh requires React 16.10 or higher. You are using React ${react.version}.`,
+          ),
+        )
+      }
 
-    console.log(chalk.cyan('Starting the development server...\n'))
-    openBrowser(urls.localUrlForBrowser)
+      console.log(chalk.cyan('Starting the development server...\n'))
+      openBrowser(urls.localUrlForBrowser)
 
-    ;['SIGINT', 'SIGTERM'].forEach(function (sig) {
-      process.on(sig, function () {
-        devServer.close()
-        process.exit()
+      ;['SIGINT', 'SIGTERM'].forEach(function (sig) {
+        process.on(sig, function () {
+          devServer.close()
+          process.exit()
+        })
       })
-    })
 
-    if (process.env.CI !== 'true') {
+      if (process.env.CI !== 'true') {
       // Gracefully exit when stdin ends
-      process.stdin.on('end', function () {
-        devServer.close()
-        process.exit()
-      })
-    }
+        process.stdin.on('end', function () {
+          devServer.close()
+          process.exit()
+        })
+      }
+    })
   })
   .catch((err) => {
     if (err && err.message) {
