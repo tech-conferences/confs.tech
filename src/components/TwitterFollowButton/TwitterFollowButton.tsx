727e4bd5db68187ac30dba43919c6a957db7b751
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-07-08 11:38:48 -0400

    improve best practice

--- src/components/TwitterFollowButton/TwitterFollowButton.tsx
+++ src/components/TwitterFollowButton/TwitterFollowButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Helmet from 'react-helmet'
 
 declare global {
@@ -10,12 +10,45 @@ declare global {
     }
   }
 }
+
 const Twitter = () => {
+  const [hasConsent, setHasConsent] = useState(false)
+
   useEffect(() => {
-    if (window.twttr) {
+    // Check if user has consented to cookies
+    const cookieConsent = localStorage.getItem('cookieConsent')
+    if (cookieConsent === 'true') {
+      setHasConsent(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (hasConsent && window.twttr) {
       window.twttr.widgets.load()
     }
-  })
+  }, [hasConsent])
+
+  // If no consent, show a simple follow link without embedded widget
+  if (!hasConsent) {
+    return (
+      <a
+        href='https://twitter.com/ConfsTech'
+        target='_blank'
+        rel='noopener noreferrer'
+        style={{
+          display: 'inline-block',
+          padding: '6px 12px',
+          backgroundColor: '#1da1f2',
+          color: 'white',
+          textDecoration: 'none',
+          borderRadius: '4px',
+          fontSize: '14px',
+        }}
+      >
+        Follow @ConfsTech
+      </a>
+    )
+  }
 
   return (
     <>
@@ -24,6 +57,7 @@ const Twitter = () => {
           async
           defer
           src='https://platform.twitter.com/widgets.js'
+          data-cookieconsent='ignore'
         ></script>
       </Helmet>
 
@@ -32,6 +66,7 @@ const Twitter = () => {
         data-show-screen-name='false'
         className='twitter-follow-button'
         data-show-count='true'
+        data-dnt='true'
       >
         Follow @ConfsTech
       </a>
