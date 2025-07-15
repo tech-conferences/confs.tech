7b64ed9dd64b0fa3a4c6a828adbd2714dccc596f
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-07-15 16:42:59 -0400

    lint

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -7,11 +7,11 @@ app.use(express.json())
 // Your static pre-build assets folder
 app.use(express.static(path.join(__dirname, '..', 'build')))
 // Root Redirects to the pre-build assets
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '..', 'build/index.html'))
 })
 // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
-app.get('*', function (req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '..', 'build/index.html'))
 })
 app.listen(port, () => {
